3a011609de0556efb39740f9d056736096441860
Evert <evert@ubuntu.(none)>
2013-09-18 15:57:22 +0200

    changed script

--- BUP/runReceiver.sh
+++ BUP/runReceiver.sh
@@ -1,5 +1,6 @@
-
+mkdir h5files -v
 rm h5files/youbottest.h5 -v
 touch h5files/youbottest.h5 
 #./H5FDdsmReceiver_cwrite_cread 10.33.172.170 22000
-./H5FDdsmReceiver_cwrite_cread 192.168.10.171 22000
+#./H5FDdsmReceiver_cwrite_cread 192.168.10.171 22000
+./H5FDdsmReceiver_cwrite_cread 10.33.174.252 22000
